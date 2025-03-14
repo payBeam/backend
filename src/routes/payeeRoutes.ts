65c4ac7d8218ca4a6baceeec307714768f28030b
Dunsin <ayodejiabisuwa23@gmail.com>
2025-03-14 19:14:02 +0100

    feat (routes) - added routes for payee

--- src/routes/payeeRoutes.ts
+++ src/routes/payeeRoutes.ts
@@ -3,10 +3,23 @@ import { handleRedirect, handleGetAuthLink } from "@/controllers/authController"
 const router = express.Router();
 
 // *-------- PAYEE ROUTES  --------------
-router.post('/create', handleGetAuthLink)
-router.put('/update')
-router.get("/:id", handleRedirect)
-router.get("/")
+/*
+    TODO - create (USDC) address to pay to tied to the invoice
+    TODO - create (USDT) address to pay to tied to the invoice
+    TODO - create (BNB) address to pay to tied to the invoice
+    TODO - create (ZETA) address to pay to tied to the invoice
+    TODO - create (ETH) address to pay to tied to the invoice
+
+*/
+
+// * tokens on Stellar
+router.post('/create/:invoiceid/usdc', handleGetAuthLink)
+router.post('/create/:invoiceid/usdt', handleGetAuthLink)
+
+// * tokens on zeta
+router.post('/create/:invoiceid/bnb', handleGetAuthLink)
+router.post('/create/:invoiceid/zeta', handleGetAuthLink)
+router.post('/create/:invoiceid/eth', handleGetAuthLink)
 
 
 
