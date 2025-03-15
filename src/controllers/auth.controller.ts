5403db8920e5805097c87003b49b0724e3b4bec5
Dunsin <ayodejiabisuwa23@gmail.com>
2025-03-15 22:30:03 +0100

    fix (controller) - added the new error and apiResponse function to it

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,25 +1,25 @@
+import { asyncHandler } from '@/middlewares/asyncHandler';
+import { ApiResponse } from '@/utils/ApiResponse';
+import { AppError } from '@/utils/AppError';
 import { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
 
 
 
-export const handleGetAuthLink = async (req: Request, res: Response, next: NextFunction) => {
+export const handleGetAuthLink = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     console.log("🔄 Redirecting to Google OAuth...");
     passport.authenticate("google", { scope: ["profile", "email"] })(req, res, next);
-};
+});
 
-export const handleRedirect = (req: Request, res: Response, next: NextFunction) => {
+export const handleRedirect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     passport.authenticate("google", { failureRedirect: "/" }, (err, user, info) => {
         if (err) return next(err); // Handle errors
-        if (!user)
-            return res.status(401).json({ mesage: "an error occured, authntication failed" }); // If authentication fails, redirect to home
+        if (!user) throw new AppError("an error occured, authntication failed", 401);
 
         // Log the user in
         req.logIn(user, (err) => {
             if (err) return next(err);
-            return res.status(200).json({
-                data: user
-            })
+            return res.status(200).json(new ApiResponse("success", user));
         });
     })(req, res, next);
-};
+});
